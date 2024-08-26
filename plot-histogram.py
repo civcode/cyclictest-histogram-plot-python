869aca8a127a2d6c89d7d18ddfa3d8af80db47cf
chris <ivi3001@yahoo.de>
2024-08-26 16:22:50 +0200

    added new feature to select cpus for plotting

--- plot-histogram.py
+++ plot-histogram.py
@@ -2,28 +2,28 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys
 import argparse
+import sys
 
-def plot_core_values(csv_file, title, save, filename):
+def plot_core_values(csv_file, title, save, filename, cpu_list):
     # Reading the CSV file into a DataFrame
-    # data = pd.read_csv(csv_file)
     data = pd.read_csv(csv_file, delim_whitespace=True, comment='#')
 
+    # Determine the number of CPU columns
+    num_cpus = len(data.columns) - 1  # Subtract 1 for the 'time' column
 
-    # Adding a header row
-    data.columns = ['time', 'core1', 'core2', 'core3', 'core4']
+    # Adding a header row dynamically
+    columns = ['time'] + [f'cpu{i}' for i in range(num_cpus)]
+    data.columns = columns
 
-    # Convert the core columns to numeric
-    data[['cpu0', 'cpu1', 'cpu2', 'cpu3']] = data[['core1', 'core2', 'core3', 'core4']].apply(pd.to_numeric)
+    # Convert the cpu columns to numeric
+    cpu_columns = [f'cpu{i}' for i in range(num_cpus)]
+    data[cpu_columns] = data[columns[1:]].apply(pd.to_numeric)
     data['time'] = data['time'].apply(pd.to_numeric)
 
-    # Convert the time and core columns to numpy arrays
+    # Convert the time and cpu columns to numpy arrays
     time = data['time'].to_numpy()
-    core1 = data['cpu0'].to_numpy()
-    core2 = data['cpu1'].to_numpy()
-    core3 = data['cpu2'].to_numpy()
-    core4 = data['cpu3'].to_numpy()
+    cpus = {f'cpu{i}': data[f'cpu{i}'].to_numpy() for i in range(num_cpus)}
 
     # Extract the min, avg, and max latencies from the CSV file
     with open(csv_file, 'r') as file:
@@ -42,29 +42,30 @@ def plot_core_values(csv_file, title, save, filename):
         elif line.startswith('# Max'):
             max_latencies = line.split(':')[1].strip().split()
 
-
-    # Plotting the values of all cores over time with a logarithmic y-axis
+    # Plotting the values of specified cpus over time with a logarithmic y-axis
     plt.figure(figsize=(12, 8))
 
-    
     # Plot statistics
     min_latencies = [str(int(x)) for x in min_latencies]
     avg_latencies = [str(int(x)) for x in avg_latencies]
-    max_latencies = [str(int(x)) for x in max_latencies]    
+    max_latencies = [str(int(x)) for x in max_latencies]
     table_header = f"cpu_ [min, avg, max]\n"
-    table_core1 = f"cpu0: [{min_latencies[0]}, {avg_latencies[0]}, {max_latencies[0]}] μs\n"
-    table_core2 = f"cpu1: [{min_latencies[1]}, {avg_latencies[1]}, {max_latencies[1]}] μs\n"
-    table_core3 = f"cpu2: [{min_latencies[2]}, {avg_latencies[2]}, {max_latencies[2]}] μs\n"
-    table_core4 = f"cpu3: [{min_latencies[3]}, {avg_latencies[3]}, {max_latencies[3]}] μs\n"
-    text_box = table_header + table_core1 + table_core2 + table_core3 + table_core4
+    text_box = table_header
+    for cpu in cpu_list:
+        if cpu in cpus:
+            i = int(cpu.split('cpu')[1])
+            text_box += f"cpu{i}: [{min_latencies[i]}, {avg_latencies[i]}, {max_latencies[i]}] μs\n"
+
     plt.text(0.5, 0.95, text_box, transform=plt.gca().transAxes, fontsize=10, verticalalignment='top', 
              horizontalalignment='center', multialignment='left', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
 
+    # Plot only the specified CPUs
+    for cpu in cpu_list:
+        if int(cpu.split('cpu')[1]) >= num_cpus:
+            print(f"Warning: CPU {cpu} exceeds the number of available CPUs.")
+        if cpu in cpus:
+            plt.plot(time, cpus[cpu], label=cpu)
 
-    plt.plot(time, core1, label='cpu0')
-    plt.plot(time, core2, label='cpu1')
-    plt.plot(time, core3, label='cpu2')
-    plt.plot(time, core4, label='cpu3')
 
     plt.yscale('log')
     plt.xlabel('Latency in µs')
@@ -84,11 +85,11 @@ if __name__ == "__main__":
     parser.add_argument('--title', type=str, default='', help='Title of the plot')
     parser.add_argument('--save', action='store_true', help='Save the plot to a file')
     parser.add_argument('--filename', type=str, default='', help='Name of the output file. If not set, will be the same as the CSV file')
+    parser.add_argument('--cpu-list', type=str, default='', help='Comma-separated list of CPU cores to plot')
 
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
-
 
     args = parser.parse_args()
 
@@ -98,5 +99,8 @@ if __name__ == "__main__":
     # Set the filename to the CSV file name if no filename is provided
     args.filename = args.filename if args.filename else args.csv_file.split('.txt')[0] + '.png'
 
-    plot_core_values(args.csv_file, args.title, args.save, args.filename)
+    # Parse the CPU list
+    cpu_list = [f'cpu{int(cpu)}' for cpu in args.cpu_list.split(',')] if args.cpu_list else [f'cpu{i}' for i in range(num_cpus)]
+
+    plot_core_values(args.csv_file, args.title, args.save, args.filename, cpu_list)
 
